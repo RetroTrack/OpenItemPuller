aa710af67633f615bbab9d3b59ec72784707ca7f
Retro <nicky.zomaar@gmail.com>
2024-10-22 18:31:50 +0200

    Fixed bug causing screen to be in the middle after reloading

--- src/main/java/com/retrotrack/openitempuller/mixin/HandledScreenMixin.java
+++ src/main/java/com/retrotrack/openitempuller/mixin/HandledScreenMixin.java
@@ -11,9 +11,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
-import net.minecraft.screen.HopperScreenHandler;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.ShulkerBoxScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
@@ -30,31 +28,28 @@ import static com.retrotrack.openitempuller.ItemPuller.MOD_ID;
 @Environment(EnvType.CLIENT)
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin extends Screen {
-    // Fields shadowed from the parent class
-    @Shadow protected int backgroundHeight;
+    // Shadow Variables
     @Final
     @Shadow protected ScreenHandler handler;
     @Shadow protected int x;
+    @Shadow protected int y;
+    @Shadow protected int backgroundWidth;
 
-    // Unique fields for this mixin
+    // Unique variables
     @Unique private TexturedButtonWidget pullButton;
-    @Unique private int lastX;
 
     protected HandledScreenMixin(Text title) {
         super(title);
     }
 
     // Injecting code to render the buttons
-    @Inject(method = "render", at = @At("TAIL"))
+    @Inject(method = "render", at = @At("HEAD"))
     public void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         // Skip rendering if the screen is a creative inventory screen
         if (this.handler instanceof CreativeInventoryScreen.CreativeScreenHandler) return;
-        // Adjust button positions if the screen has been scrolled horizontally
-        if (lastX != x) {
-            lastX = x;
-            pullButton.setPosition(this.x + 154, (this.height / 2 - this.backgroundHeight / 2
-                    - (this.handler instanceof ShulkerBoxScreenHandler || this.handler instanceof HopperScreenHandler ? 18 : 17)));
-        }
+
+        // Always reposition the button, even after another screen is closed or resized
+        repositionButton();
     }
 
     // Injecting code to initialize the buttons
@@ -62,6 +57,7 @@ public abstract class HandledScreenMixin extends Screen {
     private void init(CallbackInfo info) {
         // Skip initialization if the screen is a creative inventory screen
         if (this.handler instanceof CreativeInventoryScreen.CreativeScreenHandler) return;
+
         // Render pull button
         renderPullButton();
     }
@@ -71,11 +67,34 @@ public abstract class HandledScreenMixin extends Screen {
     private void renderPullButton() {
         // Return if client instance is null
         if (this.client == null) return;
+
         // Create pull button and attach action for checking chests
-        pullButton = this.addDrawableChild(new TexturedButtonWidget(this.x + 154,
-                (this.height / 2 - this.backgroundHeight / 2 - (this.handler instanceof ShulkerBoxScreenHandler || this.handler instanceof HopperScreenHandler ? 18 : 17)),
+        pullButton = this.addDrawableChild(new TexturedButtonWidget(
+                // The initial position is set, but we'll update it in `repositionButton`
+                this.x + this.backgroundWidth - 21,
+                this.y - 17,
                 20, 18,
-                new ButtonTextures(Identifier.of(MOD_ID, "button/pull/pull_button"/*default*/), Identifier.of(MOD_ID, "button/pull/pull_button_highlighted"/*highlighted*/)),
-                (button) -> ClientPlayNetworking.send(new CheckChestPayload(ItemPuller.CONFIG.getInteger("radius")))));
+                new ButtonTextures(
+                        Identifier.of(MOD_ID, "button/pull/pull_button" /* default */),
+                        Identifier.of(MOD_ID, "button/pull/pull_button_highlighted" /* highlighted */)
+                ),
+                (button) -> ClientPlayNetworking.send(new CheckChestPayload(ItemPuller.CONFIG.getInteger("radius")))
+        ));
+
+        // Reposition the button
+        repositionButton();
+    }
+
+    // Method to reposition the button to the top-right corner
+    @Unique
+    private void repositionButton() {
+        if (this.pullButton != null) {
+            // Calculate the new position, ensuring the button is always in the top-right corner
+            int buttonX = this.x + this.backgroundWidth - 21;
+            int buttonY = this.y - 17;
+
+            pullButton.setPosition(buttonX, buttonY);
+        }
     }
 }
+
